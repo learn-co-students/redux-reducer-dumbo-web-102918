97a027c1bcb937aa41b732489bafd5c25d60fa08
amirshohsafoev <amirsafoev0@gmail.com>
2019-01-29 12:08:33 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,26 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "REDUCE":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    case "ADD_FRIEND":
+      return {
+        friends: [
+          { name: "Avi", hometown: "NYC", id: 100 },
+          { name: "Joe", hometown: "Boston", id: 101 }
+        ]
+      };
+    case "REMOVE_FRIEND":
+      return {
+        friends: [
+          { name: "Avi", hometown: "NYC", id: 100 },
+          {
+            name: "Steven",
+            hometown: "Philadephia",
+            id: 102
+          }
+        ]
+      };
+    default:
+      return state;
+  }
 }
